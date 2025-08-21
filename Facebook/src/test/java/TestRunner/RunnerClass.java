9cd71358703993ef6b500cd7db57e2532cc302d9
madde <madde@Saloni>
2025-08-21 20:05:39 +0530

    updated 3

--- Facebook/src/test/java/TestRunner/RunnerClass.java
+++ Facebook/src/test/java/TestRunner/RunnerClass.java
@@ -5,15 +5,12 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")   // folder under src/test/resources
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefinitions")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = 
-    "pretty, html:target/cucumber-report.html, json:target/cucumber-report.json")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber-reports/Cucumber.json")
 
 public class RunnerClass {
 }
